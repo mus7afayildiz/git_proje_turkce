a47b90ca6035716db3e0a2d72fb0d02b3059d25e
mus7afayildiz <67026835+mus7afayildiz@users.noreply.github.com>
2020-06-20 22:45:54 +0300

    Update FirstClass.java
    artik cozum istiyoruz

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,6 +5,10 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
+		
+		
+		
+		
 		System.out.println("we are aganist conflict");
 		System.out.println("first commit from remote repository");
 	}
